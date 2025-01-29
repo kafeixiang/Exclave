7b210bbb117ae0baf814ca7f111e53b1eadedbc2
kafeixiang <79391973+kafeixiang@users.noreply.github.com>
2025-01-29 09:04:44 +0800

    Update StunLegacyActivity.kt

--- app/src/main/java/io/nekohasekai/sagernet/ui/StunLegacyActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/StunLegacyActivity.kt
@@ -44,24 +44,24 @@ class StunLegacyActivity : ThemedActivity() {
         binding = LayoutStunLegacyBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.toolbar)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.toolbar)) { R, insets ->
             val bars = insets.getInsets(
                 WindowInsetsCompat.Type.systemBars()
                         or WindowInsetsCompat.Type.displayCutout()
             )
-            v.updatePadding(
+            R.updatePadding(
                 top = bars.top,
                 left = bars.left,
                 right = bars.right,
             )
             WindowInsetsCompat.CONSUMED
         }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.result_layout)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.result_layout)) { R, insets ->
             val bars = insets.getInsets(
                 WindowInsetsCompat.Type.systemBars()
                         or WindowInsetsCompat.Type.displayCutout()
             )
-            v.updatePadding(
+            R.updatePadding(
                 left = bars.left,
                 right = bars.right,
                 bottom = bars.bottom,
