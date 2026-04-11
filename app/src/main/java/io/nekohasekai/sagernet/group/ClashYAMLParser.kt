b4435416ccd9b90d4331e4831661dc05d3e95713
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2026-04-12 01:17:58 +0800

    Parse Clash hysteria2 ports and hop-interval correctly
    WTF

--- app/src/main/java/io/nekohasekai/sagernet/group/ClashYAMLParser.kt
+++ app/src/main/java/io/nekohasekai/sagernet/group/ClashYAMLParser.kt
@@ -496,8 +496,12 @@ fun parseClashProxy(proxy: Map<String, Any?>): List<AbstractBean> {
         "hysteria2" -> {
             return listOf(Hysteria2Bean().apply {
                 serverAddress = proxy.getString("server") ?: return listOf()
-                serverPorts = (proxy.getString("ports")?.takeIf { it.isValidHysteriaPort() }
-                    ?: proxy.getString("port")?.toUIntOrNull()?.toString()) ?: return listOf()
+                val port = proxy.getInt("port")?.takeIf { it > 0 }
+                val ports = proxy.getString("ports")?.toIntRanges()
+                if (port == null && ports == null) return listOf()
+                serverPorts = ports?.joinToString(",") {
+                    if (it.first == it.second) it.first.toString() else it.first.toString() + "-" + it.second.toString()
+                } ?: port.toString()
                 auth = proxy.getString("password")
                 sni = proxy.getString("sni")
                 allowInsecure = proxy.getBoolean("skip-cert-verify") == true
@@ -534,7 +538,32 @@ fun parseClashProxy(proxy: Map<String, Any?>): List<AbstractBean> {
                         else -> return listOf()
                     }
                 }
-                hopInterval = proxy.getString("hop-interval")?.toUIntOrNull()?.toLong()?.takeIf { it > 0 }
+                if (!ports.isNullOrEmpty()) {
+                    // Ref: https://github.com/MetaCubeX/mihomo/commit/80072ebb6f9933e6a87ae94e3f6917a6c82c665e
+                    val hopIntervalInt = proxy.getInt("hop-interval")?.takeIf { it > 0 }
+                    val hopIntervalString = proxy.getString("hop-interval")?.toIntRange()
+                    if (hopIntervalString != null) {
+                        if (hopIntervalString.first == hopIntervalString.second) {
+                            hopInterval = hopIntervalString.first.toLong()
+                        } else {
+                            // Ref: https://github.com/MetaCubeX/mihomo/blob/80072ebb6f9933e6a87ae94e3f6917a6c82c665e/adapter/outbound/hysteria2.go#L215-L223
+                            var start = hopIntervalString.first
+                            var end = hopIntervalString.second
+                            if (start == 0) {
+                                start = 30
+                            } else if (start < 5) {
+                                start = 5
+                            }
+                            if (end < start) {
+                                end = start
+                            }
+                            hopIntervalMin = start.toLong()
+                            hopIntervalMax = end.toLong()
+                        }
+                    } else if (hopIntervalInt != null) {
+                        hopInterval = hopIntervalInt.toLong()
+                    }
+                }
                 name = proxy.getString("name")
             })
         }
@@ -909,4 +938,53 @@ private fun String.convertClashStringToInt(): Int? {
         return null
     }
     return newStr.replace("_", "").toIntOrNull()
+}
+
+// Ref: https://github.com/MetaCubeX/mihomo/blob/80072ebb6f9933e6a87ae94e3f6917a6c82c665e/common/utils/ranges.go#L16-L48
+private fun String.toIntRanges(): List<Pair<Int, Int>> {
+    // example: 200 or 200/302 or 200-400 or 200/204/401-429/501-503
+    var expected = this.trim()
+    if (expected.isEmpty() || expected == "*") {
+        return listOf()
+    }
+    // support: 200,302 or 200,204,401-429,501-503
+    expected = expected.replace(",", "/")
+    val list = expected.split("/")
+    val ranges = mutableListOf<Pair<Int, Int>>()
+    for (s in list) {
+        if (s.isEmpty()) {
+            continue
+        }
+        val r = s.toIntRange() ?: return listOf()
+        ranges.add(r)
+    }
+    return ranges
+}
+
+// Ref: https://github.com/MetaCubeX/mihomo/blob/80072ebb6f9933e6a87ae94e3f6917a6c82c665e/common/utils/range.go#L65-L87
+private fun String.toIntRange(): Pair<Int, Int>? {
+    val s = this.trim()
+    if (s.isEmpty()) {
+        return Pair(0, 0)
+    }
+    val status = s.split("-")
+    val start = status[0].trim { it == '[' || it == ' ' || it == ']' }.convertClashStringToInt()
+    if (start == null) {
+        return null
+    }
+    when (status.size) {
+        1 -> { // Port range
+            return Pair(start, start)
+        }
+        2 -> { // Single port
+            val end = status[1].trim { it == '[' || it == ' ' || it == ']' }.convertClashStringToInt()
+            if (end == null) {
+                return null
+            }
+            return Pair(start, end)
+        }
+        else -> {
+            return null
+        }
+    }
 }
