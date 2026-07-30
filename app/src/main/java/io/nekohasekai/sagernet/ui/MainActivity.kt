af03234b90f313acbcf6abaaea6b1d89a29bb817
kafeixiang <kafeixiang@example.com>
2026-07-30 22:03:31 +0800

    feat: trigger subscription update immediately after import or creation

--- app/src/main/java/io/nekohasekai/sagernet/ui/MainActivity.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/MainActivity.kt
@@ -63,6 +63,7 @@ import io.nekohasekai.sagernet.fmt.AbstractBean
 import io.nekohasekai.sagernet.fmt.Alerts
 import io.nekohasekai.sagernet.fmt.PluginEntry
 import io.nekohasekai.sagernet.group.GroupInterfaceAdapter
+import io.nekohasekai.sagernet.group.GroupUpdater
 import io.nekohasekai.sagernet.ktx.*
 import io.nekohasekai.sagernet.utils.PackageCache
 import io.noties.markwon.Markwon
@@ -300,7 +301,7 @@ class MainActivity : ThemedActivity(),
 
     private suspend fun finishImportSubscription(subscription: ProxyGroup) {
         GroupManager.createGroup(subscription)
-        // GroupUpdater.startUpdate(subscription, true)
+        GroupUpdater.startUpdate(subscription, true)
     }
 
     suspend fun importProfile(uri: Uri) {
